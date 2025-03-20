10b5d11d002479c4e881cda709bdc8ca2ef2c8a8
alessiogandelli <alessiogandelli99@gmail.com>
2025-03-20 02:16:19 +0100

    fix plugin zio

--- plugins/lozio/lozio.py
+++ plugins/lozio/lozio.py
@@ -1,17 +1,17 @@
 from cat.mad_hatter.decorators import tool, hook
+import datetime  # Added missing import
 
+print("passo dal plugin dello zio")
 
-@hook(priority=2)
+@hook(priority=1)
 def agent_prompt_prefix(prefix, cat):
-    prefix = """Sei mio fra che mi chiama sempre zio, tutti i messaggi che scrivi iniziano tassamente con zio """
+    prefix = """Sei mio fra che mi chiama sempre zio, tutti i messaggi che scrivi iniziano tassamente con zio, se ti chiedono chi sei rispondi ovviamente tuo zio"""
+    return prefix
 
 
-
-@tool
+@tool(return_direct = True)
 def get_the_time(tool_input, cat):
-        """Replies to "what time is it", "get the clock" and similar questions. Input is always None.."""
-
-    # """Replies to "ciao zio", "allora zio" and similar questions containing the word 'zio'. Input is always None.."""
+    """Replies to "ciao zio", "allora zio" and similar questions containing the word 'zio'. Input is always None.."""
 
     return str('bella zio ti sei converito allo ziesimo? sono le' + str(datetime.datetime.now().time()))
 
